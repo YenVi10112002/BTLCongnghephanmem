5bd5465b650fcd15476b2dbe85053e8c2b989432
YenVi <2051052150vi@ou.edu.vn>
2022-12-05 17:47:44 +0700

    t2 lan 2

--- QLPhongmachtu/PhongMachTu/index.py
+++ QLPhongmachtu/PhongMachTu/index.py
@@ -1,9 +1,11 @@
 from flask import render_template
 from PhongMachTu import app
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('functions/nurse.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
